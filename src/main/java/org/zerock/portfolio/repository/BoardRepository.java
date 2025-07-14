1e6f5b9e875c49e65e3122102c7ddd51e730bf7d
Han Seungbo <dnd230316@gmail.com>
2025-07-14 15:24:04 +0900

    Fix jpa

--- src/main/java/org/zerock/portfolio/repository/BoardRepository.java
+++ src/main/java/org/zerock/portfolio/repository/BoardRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.portfolio.entity.BoardEntity;
 import org.zerock.portfolio.entity.ReviewEntity;
 
@@ -15,18 +16,24 @@ public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
 //            + "left outer join ReviewEntity r on r.board = b group by b")
 //    Page<Object[]> getListPage(Pageable pageable);
 
+//    @Query("select b, bi, avg(coalesce(r.rating,0)), count(distinct r) from BoardEntity b " +
+//            "left outer join ImageEntity bi on bi.board = b " +
+//            "left outer join ReviewEntity r on r.board = b " +
+//            "group by b order by count(distinct r) desc")
+//    Page<Object[]> getPopularListPage(Pageable pageable);
+
+//    @Query("select b, bi, avg(coalesce(r.rating,0)), count(distinct r) from BoardEntity b " +
+//            "left outer join ImageEntity bi on bi.board = b " +
+//            "left outer join ReviewEntity r on r.board = b " +
+//            " where b.id = :id group by bi")
+//    List<Object[]> getBoardWithReview(Long id);
+
     @Query("select b, bi, avg(coalesce(r.rating,0)), count(distinct r) " +
             "from BoardEntity b " +
             "left outer join ImageEntity bi on bi.board = b " +
             "left outer join ReviewEntity r on r.board = b " +
             "group by b.id, b.name, b.phoneNumber, b.location, bi.id, bi.board, bi.fileName, bi.folderPath, bi.uuid")
     Page<Object[]> getListPage(Pageable pageable);
-
-//    @Query("select b, bi, avg(coalesce(r.rating,0)), count(distinct r) from BoardEntity b " +
-//            "left outer join ImageEntity bi on bi.board = b " +
-//            "left outer join ReviewEntity r on r.board = b " +
-//            "group by b order by count(distinct r) desc")
-//    Page<Object[]> getPopularListPage(Pageable pageable);
 
     @Query("select b, bi, avg(coalesce(r.rating,0)), count(distinct r) " +
             "from BoardEntity b " +
@@ -37,8 +44,9 @@ public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
     Page<Object[]> getPopularListPage(Pageable pageable);
 
     @Query("select b, bi, avg(coalesce(r.rating,0)), count(distinct r) from BoardEntity b " +
-            "left outer join ImageEntity bi on bi.board = b " +
-            "left outer join ReviewEntity r on r.board = b " +
-            " where b.id = :id group by bi")
-    List<Object[]> getBoardWithReview(Long id);
+            "left join ImageEntity bi on bi.board = b " +
+            "left join ReviewEntity r on r.board = b " +
+            "where b.id = :id group by b, bi")
+    List<Object[]> getBoardWithReview(@Param("id") Long id);
+
 }
